b18d625f1341ac8516c8224c534e4abef195407f
watta <helene.finot@le-campus-numerique.fr>
2024-08-03 18:20:10 +0200

    Add Category class and Product class

--- src/models/Category.php
+++ src/models/Category.php
@@ -1,1 +1,14 @@
 <?php
+namespace App\Models;
+
+//Category class creation with attributes
+class Category{
+    public $id;
+    public $name;
+
+    //Category class constructor
+    public function __construct($id, $name){
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
