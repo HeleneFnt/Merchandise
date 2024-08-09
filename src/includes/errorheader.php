2351d390a8f7789d5d74f4a53ba58bdcf2427a52
watta <helene.finot@le-campus-numerique.fr>
2024-08-09 23:08:28 +0200

    feature error page

--- src/includes/errorheader.php
+++ src/includes/errorheader.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-    <title>Merchandise</title>
+    <title>Error Page</title>
     <!-- My CSS Styles to adjust style -->
     <link href="assets/css/styles.css" rel="stylesheet">
 
